6a7228a191ae2c75f8da5b0190eb36558d1968c2
xiejun <1990952231@qq.com>
2026-03-06 16:20:02 +0800

    build: set base to /shuzikanban/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  base: '/shuzikanban/',
   plugins: [vue()],
   server: {
     host: true,
